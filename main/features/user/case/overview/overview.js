b90cafabf331c3b7ed3e412e676332ae1e619c74
Vincent <vincent.elcrin@gmail.com>
2014-07-09 10:48:11 +0200

    Wrap lib into an object usable in the portal.

--- main/features/user/case/overview/overview.js
+++ main/features/user/case/overview/overview.js
@@ -8,15 +8,31 @@
                     var property = parameter.split("=");
                     query[property[0]] = property[1];
                 });
-                return '/user/case/overview/' + query._id;
+                return '/user/case/overview/' + query._processDefinitionId + "/" + query._id;
             }]);
             $stateProvider.state('user/case/overview', {
-                url: '/user/case/overview/:id',
+                url: '/user/case/overview/:definitionId/:instanceId',
                 templateUrl: 'features/user/case/overview/overview.html',
                 controller: 'overviewCtrl'
             });
         }])
         .controller('overviewCtrl', ['$scope', '$stateParams', function ($scope, $stateParams) {
             $scope.case = $stateParams;
-        }]);
+        }])
+        .directive('bpmn', function() {
+            return {
+                restrict: 'E',
+                scope: {
+                    definitionId: '=',
+                    instanceId: '=',
+                    name: '='
+                },
+                template: '<svg id="bpmn-diagram" style="height:100%;width:100%"><defs></defs></svg>',
+                link: function(scope, element, attrs) {
+                    var tracker = new bonitasoft.BBPMN();
+                    console.log(scope);
+                    tracker.bootstrap(scope.definitionId, scope.instanceId, scope.name);
+                }
+            };
+        });
 })();
