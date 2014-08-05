b2f7c8701a8cbe3c2154b200b7724398e2d3479e
lombafab <fabio.lombardi@bonitasoft.com>
2014-08-05 18:36:15 +0200

    end of the dashboard

--- main/features/user/dashboard/dashboard.js
+++ main/features/user/dashboard/dashboard.js
@@ -19,24 +19,48 @@ angular.module('org.bonita.features.user.dashboard', ['org.bonita.common.resourc
                 templateUrl: 'features/user/dashboard/dashboard.html',
                 controller: 'userDashboardCtrl'
             });
-    }]).controller('userDashboardCtrl', [ '$scope', '$modal','loggedUser', 'User', 'Task', function ($scope, $modal, loggedUser, User, Task) {
+    }]).controller('userDashboardCtrl', [ '$scope', '$modal','loggedUser', 'User', 'Task', 'Process', 'Case', function ($scope, $modal, loggedUser, User, Task, Process, Case) {
         $scope.showRest = [];
         $scope.totalTasksToDo = null;
+        $scope.totalAppsAvailable = null;
+        $scope.totalCasesOpen = null;
+        $scope.firstname = null;
+        $scope.lastname = null;
 
         loggedUser.getLoggedUser().then(function(loggedUser){
-            $scope.users = User.search({
-                p: 0,
-                c: 10
+            User.get({
+                id:loggedUser.user_id
+            }).$promise.then(function(user) {
+                $scope.firstname = user.firstname;
+                $scope.lastname = user.lastname;
             });
             Task.search({
                 p:0,
                 c:5,
-                f:['user_id='+loggedUser.user_id, '&state=ready'],
+                f:['user_id='+loggedUser.user_id, 'state=ready'],
                 d:'rootContainerId'
             }).$promise.then(function(tasks) {
                 $scope.tasks = tasks.result;
                 $scope.totalTasksToDo = tasks.pagination.total;
             });
+            Process.search({
+                p:0,
+                c:5,
+                f:'user_id='+loggedUser.user_id,
+                d:'deployedBy'
+            }).$promise.then(function(apps) {
+                $scope.apps = apps.result;
+                $scope.totalAppsAvailable = apps.pagination.total;
+            });
+            Case.search({
+                p:0,
+                c:5,
+                f:'started_by='+loggedUser.user_id,
+                d:'processDefinitionId'
+            }).$promise.then(function(cases) {
+                    $scope.cases = cases.result;
+                    $scope.totalCasesOpen = cases.pagination.total;
+                });
         });
 
         $scope.hover = function(element) {
@@ -47,7 +71,7 @@ angular.module('org.bonita.features.user.dashboard', ['org.bonita.common.resourc
             return new Date(date).toLocaleString();
         }
 
-        $scope.openModal = function(size) {
+        $scope.openRestModal = function(size) {
             var modalInstance = $modal.open({
                 templateUrl: 'features/user/dashboard/taskListModalContent.html',
                 controller: ['$scope', '$modalInstance', function ($scope, $modalInstance) {
@@ -58,6 +82,25 @@ angular.module('org.bonita.features.user.dashboard', ['org.bonita.common.resourc
 
                 }],
                 size: size
+            });
+        };
+
+        $scope.openDoTask = function (name, version, processId) {
+            var dialog = $modal.open({
+                templateUrl: 'features/teamManager/users/start-process.html',
+                controller:  ['$scope', '$modalInstance', '$stateParams', '$sce', function ($scope, $modalInstance, $stateParams, $sce) {
+                    $scope.cancel = function () {
+                        $modalInstance.dismiss('cancel');
+                    };
+                    $scope.getUrl = function () {
+                        var url = $sce.trustAsResourceUrl('../portal/homepage?ui=form&locale=en&tenant=1#form=' + name + '--' + version + '$entry&process=' + processId + '&autoInstantiate=false&mode=form&userId=' + $stateParams.userId);
+                        return url;
+                    };
+                }],
+                size: 'lg'
+            });
+            dialog.result.finally(function() {
+                $scope.quickdetails.teamtasksPagination.refresh = !$scope.quickdetails.teamtasksPagination.refresh;
             });
         };
 
